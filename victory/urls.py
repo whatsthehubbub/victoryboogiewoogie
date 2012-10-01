1ae8ada35d81400850d80de932ffa04be2c28a60
Alper Çugun <git@alper.nl>
2012-10-01 22:58:48 +0200

    added twitter bootstrap

--- victory/urls.py
+++ victory/urls.py
@@ -13,3 +13,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()

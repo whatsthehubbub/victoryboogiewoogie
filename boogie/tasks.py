4af7775efab5bb6a34446527efe0b5b23faa3485
Alper Çugun <git@alper.nl>
2012-12-06 17:01:31 +0100

    Removed comment

--- boogie/tasks.py
+++ boogie/tasks.py
@@ -2,7 +2,6 @@ from celery import task
 
 @task()
 def get_new_assignment(player):
-    # TODO make another function to sweep everybody and give them new tasks
     return player.get_new_assignment()
 
 

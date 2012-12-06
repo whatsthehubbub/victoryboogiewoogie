9858f1751c410f01b5a370836a4601f2a33d05ae
Alper Çugun <git@alper.nl>
2012-12-06 20:05:07 +0100

    implemented code to display the current status for a player

--- boogie/context_processors.py
+++ boogie/context_processors.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 import datetime
 
@@ -18,14 +19,23 @@ def player(request):
             pass
 
         # TODO figure out a way to make this performant
-        if Piece.objects.filter(writer__user=request.user).filter(status='ASSIGNED'):
+
+        if returnDict['current_player'].role == 'PLAYER':
+            if Piece.objects.filter(writer__user=request.user).filter(status='ASSIGNED'):
+                returnDict['current_status'] = 'Bijdrage schrijven'
+                returnDict['current_url'] = reverse('piece_submit')
+            elif Piece.objects.filter(writer__user=request.user).filter(status='SUBMITTED'):
+                returnDict['current_status'] = 'Bijdrage in behandeling…'
+                returnDict['current_url'] = ''
+            elif Piece.objects.filter(writer__user=request.user).filter(status='NEEDSWORK'):
+                returnDict['current_status'] = 'Bijdrage verbeteren'
+                returnDict['current_url'] = reverse('piece_submit')
+            else:
+                returnDict['current_status'] = 'Wachten op opdracht…'
+                returnDict['current_url'] = ''
+        else: # Writers can always write something
             returnDict['current_status'] = 'Bijdrage schrijven'
-        elif Piece.objects.filter(writer__user=request.user).filter(status='SUBMITTED'):
-            returnDict['current_status'] = 'Bijdrage in behandeling…'
-        elif Piece.objects.filter(writer__user=request.user).filter(status='NEEDSWORK'):
-            returnDict['current_status'] = 'Bijdrage verbeteren'
-        else:
-            returnDict['current_status'] = 'Wachten op opdracht…'
+            returnDict['current_url'] = reverse('piece_submit')
 
     # TODO figure out when the game starts
     startDate = settings.GAME_START

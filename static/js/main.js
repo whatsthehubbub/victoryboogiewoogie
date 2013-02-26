3c6e49d5a3e3ef720acf63b7f8a026fe451fcb74
Simon Scheiber <mail@simonscheiber.com>
2013-02-26 19:21:21 +0100

    Logo love

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,7 @@
 // ready, go
 $(document).ready(function(){
-
-	// give input type a label
-	$("input[type='text'].label").each(function(){
-		var defaultVal = $(this).attr("title");
-		$(this).focus(function(){
-			if ($(this).val() === defaultVal){
-				$(this).removeClass("active").val("");
-			}
-		})
-		.blur(function(){
-			if ($(this).val() === ""){
-				$(this).addClass("active").val(defaultVal);
-			}
-		})
-		.blur().addClass("active");
-	});
 	
 	// fancify logo
 	$(".logo h1").lettering();
-	
-	// character counter for input field
-	$("#id_text").charCount({
-		allowed: 256,
-		warning: 50,
-		counterElement: 'div',
-		css: 'counter',
-		cssWarning: 'warning',
-		cssExceeded: 'exceeded',
-		counterPreText: 'Nog ',
-		counterPostText: ' karakters beschikbaar.'
-	});
 	
 });

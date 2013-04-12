485b981a4167ac6fb087986c9f74cbfe7ccfdd22
Kars Alfrink <kars@leapfrog.nl>
2013-04-12 09:58:15 +0200

    Added tooltip content for the various states of the write button.

--- boogie/context_processors.py
+++ boogie/context_processors.py
@@ -23,18 +23,23 @@ def player(request):
             if Piece.objects.filter(writer__user=request.user).filter(status='ASSIGNED'):
                 returnDict['current_status'] = 'Bijdrage schrijven'
                 returnDict['current_url'] = reverse('piece_submit')
+                returnDict['current_explanation'] = ''
             elif Piece.objects.filter(writer__user=request.user).filter(status='SUBMITTED'):
                 returnDict['current_status'] = 'Bijdrage in behandeling…'
                 returnDict['current_url'] = ''
+                returnDict['current_explanation'] = 'De redactie buigt zich momenteel over je ingestuurde bijdrage. Je ontvangt bericht zodra je bijdrage is goedgekeurd of geredigeerd. Een moment geduld a.u.b.'
             elif Piece.objects.filter(writer__user=request.user).filter(status='NEEDSWORK'):
                 returnDict['current_status'] = 'Bijdrage verbeteren'
                 returnDict['current_url'] = reverse('piece_submit')
+                returnDict['current_explanation'] = ''
             else:
                 returnDict['current_status'] = 'Wachten op opdracht…'
                 returnDict['current_url'] = ''
+                returnDict['current_explanation'] = 'Je ontvangt spoedig een nieuwe schrijfopdracht. Zodra dit gebeurt sturen we je een bericht.'
         else: # Writers can always write something
             returnDict['current_status'] = 'Bijdrage schrijven'
             returnDict['current_url'] = reverse('writer_piece_submit')
+            returnDict['current_explanation'] = ''
 
     try:
         game = Game.objects.get_latest_game()

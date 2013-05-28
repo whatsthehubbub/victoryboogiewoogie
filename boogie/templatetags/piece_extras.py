1780e4566dac5ecc97be6a6253ba38a4b2be0e8d
Alper Çugun <github@alper.nl>
2013-05-28 12:28:24 +0200

    made first list work properly

--- boogie/templatetags/piece_extras.py
+++ boogie/templatetags/piece_extras.py
@@ -16,3 +16,18 @@ def likes(player, piece):
 @register.filter
 def new_notifications(player):
     return Notification.objects.new_notifications_for_player(player)
+
+
+@register.filter
+def counterzerocomma(counter0, list):
+    if counter0 < len(list)-2:
+        return True
+    else:
+        return False
+
+@register.filter
+def counterzeroand(counter0, list):
+    if counter0 < len(list-1):
+        return True
+    else:
+        return False
